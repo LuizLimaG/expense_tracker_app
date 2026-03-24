09f7e7f501745ff97aef0dedd21ec08ad98e1b89
Luiz Lima <luizzfelipeliima@gmail.com>
2026-03-24 09:13:38 -0300

    feat: add welcome screen and initial app layout
    - Introduced a new welcome screen with animated elements and a login button.
    - Created a layout component to manage screen navigation.
    - Implemented a splash screen that redirects to the welcome screen after a delay.
    - Added reusable button and loading components for better UI consistency.
    - Developed a screen wrapper for consistent styling across screens.
    - Established a typography component for text styling.
    - Defined a theme file for colors and spacing constants.
    - Created type definitions for various components and data structures.
    - Updated package.json to include new dependencies for Firebase and Phosphor icons.

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,106 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import Button from "@/components/Button";
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
+
+const welcome = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <View style={{ gap: 20 }}>
+          <TouchableOpacity style={styles.loginButton}>
+            <Typo size={15} fontWeight={"500"}>
+              Login
+            </Typo>
+          </TouchableOpacity>
+          <Animated.Image
+            entering={FadeIn.duration(1400)}
+            source={require("@/assets/images/welcome.png")}
+            style={styles.welcomeImage}
+            resizeMode={"contain"}
+          />
+        </View>
+        <View style={styles.footer}>
+          <Animated.View
+            entering={FadeInDown.duration(900).springify().damping(50)}
+            style={{ alignItems: "center" }}
+          >
+            <Typo size={22} fontWeight={"800"}>
+              Always take control
+            </Typo>
+            <Typo size={22} fontWeight={"800"}>
+              of your finances
+            </Typo>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.duration(900)
+              .delay(100)
+              .springify()
+              .damping(50)}
+            style={{ alignItems: "center", gap: 2 }}
+          >
+            <Typo size={14} color={colors.textLight}>
+              Finances would be arranged to set a
+            </Typo>
+            <Typo size={14} color={colors.textLight}>
+              better lifestyle in future
+            </Typo>
+          </Animated.View>
+
+          <Animated.View
+            entering={FadeInDown.duration(900)
+              .delay(200)
+              .springify()
+              .damping(50)}
+            style={styles.buttonContainer}
+          >
+            <Button>
+              <Typo size={18} color={colors.neutral900} fontWeight={"600"}>
+                Get started
+              </Typo>
+            </Button>
+          </Animated.View>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingTop: spacingY._7,
+  },
+  welcomeImage: {
+    width: "100%",
+    height: verticalScale(300),
+    alignSelf: "center",
+  },
+  loginButton: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+  footer: {
+    backgroundColor: colors.neutral900,
+    alignItems: "center",
+    paddingTop: verticalScale(30),
+    paddingBottom: verticalScale(45),
+    gap: spacingY._20,
+    shadowColor: "white",
+    shadowOffset: { width: 0, height: -10 },
+    elevation: 100,
+    shadowRadius: 10,
+    shadowOpacity: 0.7,
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: spacingX._25,
+  },
+});
